6b1c8ea619057c99ee0f5353e61ddc4681e50ba4
Chris <coderchrismills@gmail.com>
2018-12-10 20:59:38 -0800

    Initial Commit

---
+++ CCStore/CCStore.h
@@ -0,0 +1,19 @@
+//
+//  CCStore.h
+//  CCStore
+//
+//  Created by Chris on 12/10/18.
+//  Copyright © 2018 Chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CCStore.
+FOUNDATION_EXPORT double CCStoreVersionNumber;
+
+//! Project version string for CCStore.
+FOUNDATION_EXPORT const unsigned char CCStoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CCStore/PublicHeader.h>
+
+
